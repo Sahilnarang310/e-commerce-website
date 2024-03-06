dd023e65c2efd43504a0b4a797bd840601eb60ce
Sahilnarang310 <softwaredev310@gamil.com>
2024-03-06 20:33:01 +0530

    cart component added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Layout from './components/Layout/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/react-bootstrap/dist/react-bootstrap';
+import Cart from './components/Cart'
 
 function App() {
   return (
     <>
     <Layout/>
+    <Cart />
     </>
   );
 }
