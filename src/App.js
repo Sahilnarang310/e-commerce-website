efe69966dd752e50886d4a19b3ffbf8ca0913802
Sahilnarang310 <softwaredev310@gamil.com>
2024-03-07 11:15:53 +0530

    making api call in react

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Layout from './components/Layout/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/react-bootstrap/dist/react-bootstrap';
 import Cart from './components/Cart'
+import Pages from "./components/Pages"
 
 function App() {
   return (
     <>
     <Layout/>
     <Cart />
+    <Pages />
     </>
   );
 }
